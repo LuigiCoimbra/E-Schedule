db998f3297f732af9f5f30e511c0dc8856b5fe8c
LuigiCoimbra <53053670+LuigiCoimbra@users.noreply.github.com>
2021-11-25 10:51:14 -0300

    Add files via upload

---
+++ src/pages/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-page>
+    <div class="q-pa-md">
+      <q-table
+        title="Treats"
+        :rows="rows"
+        :columns="columns"
+        row-key="name"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      columns: [
+        {
+          name: 'title',
+          field: 'title',
+          required: true,
+          label: 'Titulo',
+          align: 'left',
+          sortable: true
+        }
+      ],
+      rows: [
+        {
+          title: 'Anotação 1'
+        },
+        {
+          title: 'Anotação 2'
+        }
+      ]
+    }
+  }
+}
+</script>
